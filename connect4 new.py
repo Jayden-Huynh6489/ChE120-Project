16b88fd02ba150a4ba10896287bdbc205d349a03
NicoAyelo <117311499+NicoAyelo@users.noreply.github.com>
2022-12-05 17:53:03 -0500

    Update connect4 new.py

--- connect4 new.py
+++ connect4 new.py
@@ -2,58 +2,46 @@
 """
 Created on Wed Nov  9 15:45:57 2022
 
-@author: Charlie, Grace ,Jayden, Nicolas: N.A.
+@author: Charlie, Grace ,Jayden, Nicolas
 """
 
 
-#Here is where specific modules are being imported. We see a familiar one from the lectures. N.A.
 import numpy as np
 import pygame
 import sys
 import math
 
-#These are the colours of board, background, red chips, and yellow chips. They numbers in the brackets correspond to the 
-#RGB colour model. In the brackets it goes (red,green,blue). N.A.
+
 BLUE = (0,0,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
 YELLOW = (255,255,0)
 
-#Here we have the number of rows (6) and number of columns (7) being created N.A.
 ROW_COUNT = 8#initize row variable
 COLUMN_COUNT =9 #initize column variable
- 
-#Here, a function for creating the playing board is being defined. The board is made using the module numpy as np, and 
-#the previous variables corresponding to the number of rows and columns. N.A.
+
 
 def create_board():
-    #this function is to in initize a board. at first the board will be empty N.A.
   
     board = np.zeros((ROW_COUNT,COLUMN_COUNT))
     return board
  
-#The function defined here is to drop the pieces into the board, at specific row and column locations. N.A.
   
 def drop_piece(board, row, col, piece):
     board[row][col] = piece
-#Next, this function is meant to check if a piece was placed in a valid location. There must be nothing in that 
-#location, hence the '0'. N.A.
+
 def is_valid_location(board, col):
     return board[ROW_COUNT-1][col] == 0
 
-# in the next few function definitions, r refers to row, and c refers to column
-#This function is meant to place a piece in the next open row? N.A.
+
 def get_next_open_row(board, col):
     for r in range(ROW_COUNT):
         if board[r][col] == 0:
             return r
-#The defined function here is used to print the board when changes are made to it (updated with added pieces shown). N.A.
 def print_board(board):
     print(np.flip(board, 0))
 
-#This function is meant to check if there is 4 in a row horizontally. It checks if there are 4 pieces in a row from an 
-#initial position c (that's why there are things like c+1, c+2, c+3). r must be constant for this to be the case because 
-#it only counts horizontally. If there are 4 in a row horizontally, then True is returned and there is a winner. N.A.
+
 def winning_move(board, piece):
   #Check horizontal locations for win
   i=0
@@ -64,10 +52,6 @@ def winning_move(board, piece):
                 if i==2:
                     return True
  
-#This function acts like the previous one, except this time it check if there are 4 in a row vertically. 
-#There must be 4 pieces from an initial position r, and then r+1, r+2, r+3 all must follow. c is 
-#constant because only the rows are being counted, not the columns True is returned if there are 4 in a row vertically and there is a winner. N.A.
-# Check vertical locations for win
               
   for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT-3):
@@ -76,10 +60,6 @@ def winning_move(board, piece):
                 if i==2:
                     return True
  
-#This function, like the previous two, checks if there are 4 pieces in a row. This time it is diagonally upward though (think like a positively sloped graph).
-#To do this it counts one to the left or right from c, and then one up from row. If there are 4 in row with a positive slope, then True is returned and there is 
-#a winner. N.A.
-#Check positively sloped diaganols
     
   for c in range(COLUMN_COUNT-3):
         for r in range(ROW_COUNT-3):
@@ -88,8 +68,6 @@ def winning_move(board, piece):
                 if i==2:
                     return True
  
-#This function is like the last one except it counts diagonally downward (with like a negative slope). The only difference is that it counts down 
-#from initial r position. N.A.
 # Check negatively sloped diaganols
   for c in range(COLUMN_COUNT-3):
         for r in range(3, ROW_COUNT):
@@ -131,8 +109,7 @@ def winning_move2(board, piece):
                 if i==2:
                     return True
  
-#This function is like the last one except it counts diagonally downward (with like a negative slope). 
-#The only difference is that it counts down from initial r position. N.A.
+
 # Check negatively sloped diaganols
   for c in range(COLUMN_COUNT-3):
         for r in range(3, ROW_COUNT):
